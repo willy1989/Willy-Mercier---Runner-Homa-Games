080989d6eeefa09fe1cc9fe5efe45fb47310121e
Willy <willy.winelover@gmail.com>
2022-01-26 11:31:30 +0100

    Created floor and character gameobjects

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ControlInputs : MonoBehaviour
+public class InputManager : MonoBehaviour
 {
     private Vector2 startTouchPosition;
 
